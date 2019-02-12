8f4c50b1d9063e179e2d178b3a78699867cef3a6
torstenzey <46266432+torstenzey@users.noreply.github.com>
2019-02-12 20:24:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,34 @@
-# Bindings for the hermes protocol 
-hermes-python>=0.1
-xmltodict
-paho-mqtt
-future==0.16
+#/usr/bin/env bash -e
+
+PYTHON=`which python3`
+VENV=venv
+
+if [ -f "$PYTHON" ]
+then
+
+    if [ ! -d $VENV ]
+    then
+        # Create a virtual environment if it doesn't exist.
+        $PYTHON -m venv $VENV
+    else
+        if [ -e $VENV/bin/python2 ]
+        then
+            # If a Python2 environment exists, delete it first
+            # before creating a new Python 3 virtual environment.
+            rm -r $VENV
+            $PYTHON -m venv $VENV
+        fi
+    fi
+
+    # Activate the virtual environment and install requirements.
+    . $VENV/bin/activate
+    pip3 install -r requirements.txt
+
+else
+    >&2 echo "Cannot find Python 3. Please install it."
+fi
+
+if [ ! -f ./.favs ]; then
+    touch .favs
+    sudo chown _snips-skills .favs
+fi
