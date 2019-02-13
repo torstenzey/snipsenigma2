1f82d43fc3893c650e6bd6a98213cb5b878534ab
torstenzey <46266432+torstenzey@users.noreply.github.com>
2019-02-13 21:24:11 +0100

    Update action-enigma2.py

--- action-enigma2.py
+++ action-enigma2.py
@@ -6,6 +6,7 @@ import json
 import httplib
 
 def on_connect(client, userdata, flags, rc):
+    mqtt.subscribe('hermes/intent/torstenzey:switch_to')
     mqtt.subscribe('hermes/intent/torstenzey:television_on')
     mqtt.subscribe('hermes/intent/torstenzey:television_off')
     mqtt.subscribe('hermes/intent/torstenzey:volume_down')
@@ -17,7 +18,16 @@ def on_message(client, userdata, msg):
     intent_json = json.loads(msg.payload)
     intentName = intent_json['intent']['intentName']
     slots = intent_json['slots']
+    # slot = slots[0]['value']['value']
     print('Intent {}'.format(intentName))
+  
+    
+
+    for slot in slots:
+        slot_name = slot['slotName']
+        raw_value = slot['rawValue']
+        value = slot['value']['value']
+        print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_name, raw_value, value))
 
     con = httplib.HTTPConnection('192.168.178.206')
     if 'torstenzey:television_on' == intentName :
@@ -29,20 +39,17 @@ def on_message(client, userdata, msg):
     elif 'torstenzey:volume_up' == intentName :
         con.request("GET", "/web/vol?set=up")
     elif 'torstenzey:zap_forward' == intentName :
-        con.request("GET", "/web/remotecontrol?command=407")
+        con.request("GET", "/web/remotecontrol?command=106")
     elif 'torstenzey:zap_forward' == intentName :
-        con.request("GET", "/web/remotecontrol?command=412")
+        con.request("GET", "/web/remotecontrol?command=105")
+    elif 'torstenzey:switch_to' == intentName and 'ARD' == slot :
+        con.request("GET", "/web/remotecontrol?command=105")
 
     print(con.getresponse().status)
 
     con.close()
     
-    for slot in slots:
-        slot_name = slot['slotName']
-        raw_value = slot['rawValue']
-        value = slot['value']['value']
-        print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_name, raw_value, value))
-
+ 
 mqtt = mqtt.Client()
 mqtt.on_connect = on_connect
 mqtt.on_message = on_message
